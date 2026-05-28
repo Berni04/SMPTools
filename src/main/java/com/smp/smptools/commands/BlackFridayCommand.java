f86144be5b61def87fdf4190d9ed423dc72cf8cc
Berni04 <37367381+Berni04@users.noreply.github.com>
2026-05-29 00:21:18 +0200

    Refactor: Replace ChatColor with Adventure API (Part 13)
    - BlackFridayCommand: Use Component.text() with NamedTextColor

--- src/main/java/com/smp/smptools/commands/BlackFridayCommand.java
+++ src/main/java/com/smp/smptools/commands/BlackFridayCommand.java
@@ -1,7 +1,8 @@
 package com.smp.smptools.commands;
 
 import com.smp.smptools.managers.BlackFridayManager;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,7 +18,7 @@ public class BlackFridayCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.isOp()) {
-            sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+            sender.sendMessage(Component.text("You don't have permission to use this command!", NamedTextColor.RED));
             return true;
         }
 
@@ -31,28 +32,29 @@ public class BlackFridayCommand implements CommandExecutor {
             case "enable":
                 manager.setEnabled(true);
                 manager.broadcastToggle(true);
-                sender.sendMessage(ChatColor.GREEN + "Black Friday event has been enabled!");
+                sender.sendMessage(Component.text("Black Friday event has been enabled!", NamedTextColor.GREEN));
                 break;
 
             case "stop":
             case "disable":
                 manager.setEnabled(false);
                 manager.broadcastToggle(false);
-                sender.sendMessage(ChatColor.RED + "Black Friday event has been disabled!");
+                sender.sendMessage(Component.text("Black Friday event has been disabled!", NamedTextColor.RED));
                 break;
 
             case "reload":
                 manager.reloadConfig();
-                sender.sendMessage(ChatColor.GREEN + "Black Friday configuration reloaded!");
+                sender.sendMessage(Component.text("Black Friday configuration reloaded!", NamedTextColor.GREEN));
                 break;
 
             case "status":
                 boolean enabled = manager.isEnabled();
                 int discount = manager.getDiscountPercentage();
-                sender.sendMessage(ChatColor.GOLD + "=== Black Friday Status ===");
-                sender.sendMessage(
-                        ChatColor.YELLOW + "Enabled: " + (enabled ? ChatColor.GREEN + "YES" : ChatColor.RED + "NO"));
-                sender.sendMessage(ChatColor.YELLOW + "Discount: " + ChatColor.AQUA + discount + "%");
+                sender.sendMessage(Component.text("=== Black Friday Status ===", NamedTextColor.GOLD));
+                sender.sendMessage(Component.text("Enabled: ", NamedTextColor.YELLOW)
+                        .append(Component.text(enabled ? "YES" : "NO", enabled ? NamedTextColor.GREEN : NamedTextColor.RED)));
+                sender.sendMessage(Component.text("Discount: ", NamedTextColor.YELLOW)
+                        .append(Component.text(discount + "%", NamedTextColor.AQUA)));
                 break;
 
             default:
@@ -64,10 +66,14 @@ public class BlackFridayCommand implements CommandExecutor {
     }
 
     private void sendHelp(CommandSender sender) {
-        sender.sendMessage(ChatColor.GOLD + "=== Black Friday Commands ===");
-        sender.sendMessage(ChatColor.YELLOW + "/blackfriday start" + ChatColor.GRAY + " - Enable the event");
-        sender.sendMessage(ChatColor.YELLOW + "/blackfriday stop" + ChatColor.GRAY + " - Disable the event");
-        sender.sendMessage(ChatColor.YELLOW + "/blackfriday reload" + ChatColor.GRAY + " - Reload configuration");
-        sender.sendMessage(ChatColor.YELLOW + "/blackfriday status" + ChatColor.GRAY + " - View event status");
+        sender.sendMessage(Component.text("=== Black Friday Commands ===", NamedTextColor.GOLD));
+        sender.sendMessage(Component.text("/blackfriday start", NamedTextColor.YELLOW)
+                .append(Component.text(" - Enable the event", NamedTextColor.GRAY)));
+        sender.sendMessage(Component.text("/blackfriday stop", NamedTextColor.YELLOW)
+                .append(Component.text(" - Disable the event", NamedTextColor.GRAY)));
+        sender.sendMessage(Component.text("/blackfriday reload", NamedTextColor.YELLOW)
+                .append(Component.text(" - Reload configuration", NamedTextColor.GRAY)));
+        sender.sendMessage(Component.text("/blackfriday status", NamedTextColor.YELLOW)
+                .append(Component.text(" - View event status", NamedTextColor.GRAY)));
     }
 }
